03d587855b378178eb3786d2683cf61a83430ab3
ahmbilgili <ahm.bilgili@gmail.com>
2026-04-18 15:14:30 +0000

    Quick hotfix

--- config.py
+++ config.py
@@ -1,7 +1,10 @@
 from dotenv import load_dotenv
 import os
-load_dotenv(dotenv_path="prod.env")
 
+if os.getenv("IN_PROD"):
+    load_dotenv(dotenv_path="prod.env")
+else:
+    load_dotenv(dotenv_path="../dev_localtest.env")
 
 class Config:
     SECRET_KEY = os.getenv("WTF_CSRF_SECRET_KEY")
