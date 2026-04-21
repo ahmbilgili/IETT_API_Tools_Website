a4b9a2e92cc3c2b95c6f110bf57d07fe661edbfa
ahmbilgili <ahm.bilgili@gmail.com>
2026-04-21 18:08:36 +0000

    -Added rate limiting

--- app.py
+++ app.py
@@ -9,8 +9,11 @@ from line_service.app import line_service_bp
 from feedback.app import feedback_bp
 from captcha.image import ImageCaptcha
 import random
+import redis
+import time
 
 random.seed(10)
+os.environ["IN_TEST_CLOUD"] = "True"
 
 
 app = Flask(__name__)
@@ -29,6 +32,55 @@ app.register_blueprint(feedback_bp)
 csrf = CSRFProtect(app=app)
 csrf.init_app(app)
 
+# DB Credentials
+REDIS_DB_IP = os.getenv("REDIS_DB_IP")
+REDIS_DB_PORT = os.getenv("REDIS_DB_PORT")
+REDIS_DB_USER = os.getenv("REDIS_DB_USER")
+REDIS_DB_PASSWORD = os.getenv("REDIS_DB_PASSWORD")
+
+# Rate limiter config
+RATE_LIMIT = 30
+RATE_LIMIT_WINDOW = 60
+
+redis_client = redis.Redis(
+    host=REDIS_DB_IP,
+    port=REDIS_DB_PORT,
+    decode_responses=True,
+    username=REDIS_DB_USER,
+    password=REDIS_DB_PASSWORD
+)
+
+@app.before_request
+def vibe_check():
+    key = request.remote_addr
+    curr_time = int(time.time())
+    
+    # pipe that can queue multiple commands for later execution
+    pipe = redis_client.pipeline()
+
+    pipe.zadd(key, {f"now: {curr_time}": curr_time})
+
+    if redis_client.zcard(key) == 0:
+        # Setting exp date on key
+        pipe.expire(key, RATE_LIMIT_WINDOW)
+
+    # Removing records that are older than (current_time - rate_limit_window)
+    # pipe.zremrangebyrank(key, min=0, max=curr_time - RATE_LIMIT_WINDOW)
+
+    # Get the current # of requests
+    pipe.zcard(key)
+
+    result = pipe.execute()
+    
+    if len(result) == 3:
+        num_of_req = result[2]
+    else:
+        num_of_req = result[1]
+
+    print(num_of_req)
+    if num_of_req >= RATE_LIMIT:
+        return Response("You've sent too many requests in a short period of time, please wait until cooldown period ends", status=429)
+    
 @app.route("/", methods=['GET'])
 def base_handler():
     return render_template("base.html")
--- config.py
+++ config.py
@@ -3,8 +3,13 @@ import os
 
 if os.getenv("IN_PROD"):
     load_dotenv(dotenv_path="prod.env")
+    print("loading prod config")
+elif os.getenv("IN_TEST_CLOUD"):
+    load_dotenv(dotenv_path="../dev_cloudtest.env")
+    print("loading cloud test config")
 else:
     load_dotenv(dotenv_path="../dev_localtest.env")
+    print("loading local test config")
 
 class Config:
     SECRET_KEY = os.getenv("WTF_CSRF_SECRET_KEY")
